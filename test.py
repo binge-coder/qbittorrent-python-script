3729ae1716c3367161d050551f02dae8fa9b2b1c
KaranKanwar <kkanwar120502@gmail.com>
2022-04-17 22:22:47 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,33 @@
+import qbittorrentapi
+
+qbt_client = qbittorrentapi.Client(host = "localhost:8080", username = "admin", password = "swagsadadesi")
+
+try:
+    qbt_client.auth_log_in()
+except qbittorrentapi.LoginFailed as e:
+    print(e)
+'''
+print(f'qBittorrent: {qbt_client.app.version}')
+print(f'qBittorrent Web API: {qbt_client.app.web_api_version}')
+for k, v in qbt_client.app.build_info.items():
+    print(f'{k}: {v}')
+'''
+
+'''
+for torrent in qbt_client.torrents_info():
+    print(f'{torrent.hash[-6:]}: {torrent.name} ({torrent.state})')
+'''
+
+'''
+#pause all torrents
+qbt_client.torrents.pause.all()
+
+'''
+qbt_client.log.main.warning()
+qbt_client.log.main.normal()
+
+
+
+
+
+
