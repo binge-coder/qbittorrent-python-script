6ce7669c5f315a6b6289d0653e72f4ef9c1d9056
KaranKanwar <kkanwar120502@gmail.com>
2022-04-19 14:59:03 +0530

    added to-do inside test.py

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+''' 
+to do :
+. set limit to 200 mb
+. read files and check size
+
+procedure:
+. read file size in the beginning 
+. refresh file size and check difference . if difference == 200 mb (200,000,000 Bytes)
+
+
+'''
+
 from decouple import config     #to get login details from .env 
 import qbittorrentapi
+import os # to get the file size
 
 
 qbt_client = qbittorrentapi.Client(host = "localhost:8080", username = config('USER'), password = config('PASSWORD'))
@@ -25,9 +38,12 @@ for torrent in qbt_client.torrents_info():
 qbt_client.torrents.pause.all()
 
 '''
+def getsize()
+{
+    
+}
 
-qbt_client.log.main.warning()
-qbt_client.log.main.normal()
+
 
 
 
