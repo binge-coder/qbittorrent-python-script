c2aacedd724a53d66be1ebab3f3f1d6489e87f0b
kk_in_mint <kkanwar120502@gmail.com>
2022-05-30 21:22:29 +0530

    "nothing changed"

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
 '''
-
-to do :
-. set limit to 200 mb
-. read files and check size
-
 procedure:
 . read file size in the beginning 
 . refresh file size and check difference . if difference == 200 mb (200,000,000 Bytes)
@@ -12,9 +7,12 @@ procedure:
 
 from decouple import config     #to get login details from .env 
 import qbittorrentapi
-import os # to get the file size
 
-shutdown = 0
+total_download_limit_MB =  500
+total_download_limit_BYTES = total_download_limit_MB * (1024) * (1024)
+total_downloaded_in_session = 0
+
+# shutdown = False
 qbt_client = qbittorrentapi.Client(host = "localhost:8080", username = config('USER'), password = config('PASSWORD'))
 
 try:
@@ -22,10 +20,20 @@ try:
 except qbittorrentapi.LoginFailed as e:
     print(e)
 
-'''
+
+# for torrent in qbt_client.torrents_info():
+#     print(f'{torrent.hash[-6:]}: {torrent.name} ({torrent.state})({torrent.downloaded})')
+
+for x in qbt_client.torrents_info():
+    print(f"{x}\n")
+
 for torrent in qbt_client.torrents_info():
-    print(f'{torrent.hash[-6:]}: {torrent.name} ({torrent.state})')
-'''
+    if(torrent.state == 'downloading'):
+        total_downloaded_in_session = total_downloaded_in_session + torrent.downloaded_session
+
+if(total_downloaded_in_session > total_download_limit_BYTES):
+    qbt_client.torrents.pause.all()
+    
 
 '''
 #pause all torrents
@@ -39,11 +47,12 @@ def getsize():
 
 
 # logging out
-qbittorrentapi.auth_log_out()
+# qbittorrentapi.auth_log_out()
 
+"""
 # shutdown the computer
-if shutdown == 1:
+if shutdown == True: #made if condition so that i dont shut it down by mistake
     os.system("shutdown /s /d[u:]  /t 60 ") #this may not work or may malfunction
-
+"""
 
 
